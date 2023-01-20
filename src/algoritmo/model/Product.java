a663d34afee571272e0a47fc0cdf77cea6f1276f
Thalles89 <thallesamott@gmail.com>
2023-01-20 19:27:17 -0300

    Just updating everything, again

---
+++ src/algoritmo/model/Product.java
@@ -0,0 +1,31 @@
+package algoritmo.model;
+
+import jgap.Evaluable;
+
+public class Product implements Evaluable {
+
+    private final String name;
+    private final Double valor;
+    private final Double volume;
+
+    public Product(String name, Double volume, Double valor) {
+        super();
+        this.name = name;
+        this.valor = valor;
+        this.volume = volume;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public Double getScore() {
+        return valor;
+    }
+
+    @Override
+    public Double getTarget() {
+        return volume;
+    }
+}
